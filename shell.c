0342e07694d390f74e27bd849f50e37433b9496c
Nihad <nihadamirov98@gmail.com>
2024-05-01 12:39:06 +0000

    modified file name

---
+++ shell.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function serves as the entry point of the shell program.
+ * It reads commands from the standard input, executes them, and prints the
+ * results to the standard output.
+ *
+ * Return: Always returns 0 to indicate successful completion.
+ */
+int main(void)
+{
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	ssize_t characters;
+
+	while (1)
+	{
+		printf("$ ");
+		characters = custom_getline(&buffer, &bufsize);
+		if (characters == -1)
+		{
+			break;
+		}
+
+		buffer[strcspn(buffer, "\n")] = '\0';
+		execute_command(buffer);
+		free(buffer);
+		buffer = (NULL);
+	}
+	if (buffer != NULL)
+	{
+		free(buffer);
+	}
+	return (0);
+}
