4630e90d73bd24909f8c2d56992f1b989db2c2be
Nihad <nihadamirov98@gmail.com>
2024-05-04 19:44:45 +0000

    modified file

--- shell.h
+++ shell.h
@@ -2,15 +2,21 @@
 #define SHELL_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 
-/* Function prototypes */
-ssize_t custom_getline(char **lineptr, size_t *n);
-void execute_command(char *command);
-
-#endif /* SHELL_H */
-
+char **str_tokens(char *str);
+int create_fork(char *shell_name, char **tokens, char **env, int is_terminal);
+int special_cases(char **tokens, char *shell_name, char **env);
+char *get_path(char **env);
+char *split_path(char *file_token, char *path);
+int _strlen(char *s);
+int _strcmp(char *a, char *b);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+#endif
