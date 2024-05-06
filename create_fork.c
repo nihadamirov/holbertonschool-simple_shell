320fc7b8cb83b8a5dbe7c8d2051c8b623bc89da9
Nihad <nihadamirov98@gmail.com>
2024-05-06 14:42:37 +0000

    update file

--- create_fork.c
+++ create_fork.c
@@ -2,65 +2,40 @@
 
 int create_fork(char *shell_name, char **tokens, char **env, int is_terminal)
 {
-	pid_t child_pid;
-	int status;
+    pid_t child_pid;
+    int status;
 
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("Error:");
-		return (0);
-	}
-	else if (child_pid == 0)
-	{
-		char *full_file_name;
-		char *path;
+    child_pid = fork();
+    if (child_pid == -1)
+    {
+        perror("Fork error");
+        return (0);
+    }
+    else if (child_pid == 0)
+    {
+        execve(tokens[0], tokens, env);
 
-		if (!tokens || !tokens[0])
-			return (0);
+        perror("Execve error");
 
-		execve(tokens[0], tokens, env);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        wait(&status);
 
-		/*
-		 * Everything past the above line is
-		 * due to an executable not being found.
-		 *
-		 * if the stdin wasn't empty:
-		 */
-
-		path = get_path(env);
-		full_file_name = split_path(tokens[0], path);
-
-		if (full_file_name != NULL && path != NULL)
-		{
-			char *exec_name = tokens[0];
-
-			tokens[0] = full_file_name;
-			execve(tokens[0], tokens, env);
-			tokens[0] = exec_name;
-			free(full_file_name);
-		}
-
-		if (tokens != NULL && *tokens != NULL)
-		{
-			write(1, shell_name, _strlen(shell_name));
-
-			if (is_terminal)
-				write(1, ": No such file or directory\n", 28);
-			else
-			{
-				write(1, ": 1: ", 5);
-				write(1, tokens[0], _strlen(tokens[0]));
-				write(1, ": not found\n", 12);
-			}
-		}
-
-		return (0);
-	}
-	else
-	{
-		wait(&status);
-	}
-	return (1);
+        if (!WIFEXITED(status))
+        {
+            write(1, shell_name, _strlen(shell_name));
+            if (is_terminal)
+                write(1, ": No such file or directory\n", 28);
+            else
+            {
+                write(1, ": 1: ", 5);
+                write(1, tokens[0], _strlen(tokens[0]));
+                write(1, ": not found\n", 12);
+            }
+        }
+    }
+    return (1);
 }
 
