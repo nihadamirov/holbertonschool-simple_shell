914e64f26448fc730f84a54e102081f72afd4d30
Nihad <nihadamirov98@gmail.com>
2024-05-06 09:06:20 +0000

    update file

--- token.c
+++ token.c
@@ -1,50 +1,41 @@
 #include "shell.h"
+#include <string.h>
 
 /**
- * str_tokens - Returns an array of pointers
- * to individual tokens in the 'str'
- * using "strtok(stdin_line, " \n")".
+ * str_tokens - Tokenizes the input string into individual tokens.
  * @str: The string to be tokenized.
  *
- * The returned array is null poiinter-terminated.
- * It's also malloc'ed, and needs to be freed.
- * If it fails to malloc or if 'str is NULL,
- * str_tokens returns  NULL.
+ * This function tokenizes the input string 'str' using
+ * the "strtok" function, with delimiters " \n".
+ * It returns an array of pointers to individual tokens.
+ * The returned array is null pointer-terminated.
+ * It's also allocated dynamically using malloc,
+ * and must be freed by the caller.
+ * If the malloc call fails or if 'str' is NULL,
+ * str_tokens returns NULL.
  *
- * Return: NULL pointer-terminated array of
- * pointers to tokens in the 'str' string
- * if the malloc call was successful,
- * NULL otherwise
+ * Return: NULL-terminated array of pointers to tokens in the 'str' string
+ * if the malloc call was successful, NULL otherwise.
  */
 char **str_tokens(char *str)
 {
 	char **result;
 	char *token;
-	/*
-	 * malloc token pointer array with the same length
-	 * as 'str' to ensure an overflow doesn't happen:
-	 */
-	int str_len;
 	int result_index = 0;
 
 	if (!str)
 		return (NULL);
 
-	str_len = _strlen(str);
-
-	result = malloc(sizeof(char *) * (str_len + 1));
+	result = malloc(sizeof(char *) * (strlen(str) + 1));
 	if (!result)
 		return (NULL);
 
-	token = strtok(str, " \n");
-	while (token)
+	while ((token = strsep(&str, " \n")) != NULL)
 	{
-		result[result_index] = token;
-		result_index++;
-		token = strtok(NULL, " \n");
+		result[result_index++] = token;
 	}
+
 	result[result_index] = NULL;
 
 	return (result);
 }
-
